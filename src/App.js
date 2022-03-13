0cebf299c742eaa3587a81a11f300d6a696a30a0
alia_ioanu <aliuwe4ka@gmail.com>
2022-03-13 19:38:12 +0300

    added ChatEngine

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Login from './Login'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {ChatEngine} from "react-chat-engine";
 
 
 function App() {
@@ -9,6 +10,13 @@ function App() {
       <Router>
         <Routes>
             <Route path='/' element={<Login />} />
+            <Route path='/main' element={<ChatEngine
+                height='100vh'
+                projectID='f975b43d-706d-46d0-b73a-b7c11e1f3111'
+                userName='janeostin'
+                userSecret='123'
+            />}
+            />
         </Routes>
       </Router>
     </div>
