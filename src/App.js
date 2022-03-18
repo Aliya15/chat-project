2ea2407ac8947b771bca75011b375130ca528d23
alia_ioanu <aliuwe4ka@gmail.com>
2022-03-18 11:16:35 +0300

    implemented login config

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import Login from './components/Login'
 import ChatRoom from './components/ChatRoom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { auth } from "../src/firebaseConfig";
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 function App() {
+    const [user] = useAuthState(auth);
   return (
     <div className='App'>
       <Router>
         <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/main' element={<ChatRoom/>} />
+            <Route path='/' element={user ? <ChatRoom/> : <Login />} />
         </Routes>
       </Router>
     </div>
